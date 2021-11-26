8cc0e7605a50bc6d1e533b2fcb0845fbac6a6c93
Anna Bavtruk <ann.bavtruk@gmail.com>
2021-11-26 23:26:10 +0300

    create app

---
+++ coffee/src/components/Layout.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Landing from './landing/Landing';
+import {
+    BrowserRouter, 
+    BrowserRouter as Router, 
+    Switch,
+    Route
+} from "react-router-dom";
+
+
+
+export default function Layout() {
+   return(
+       <BrowserRouter>
+       <Switch>
+           <Route exact path='/' render={()=><Landing />}/>
+       </Switch>
+       </BrowserRouter>
+   
+    )
+}
